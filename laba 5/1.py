2b0c81c7816446ee2a2ac3d37ae1c87a2c080395
KenyaMolkova <159025365+KenyaMolkova@users.noreply.github.com>
2024-04-29 00:03:51 +0300

    Update 1.py

--- laba 5/1.py
+++ laba 5/1.py
@@ -10,17 +10,3 @@ if s in a:
 else:
     print(a, s, "Нет такого числа!")
 
-#2
-import random
-a = []
-for i in range(5):
-    a.append(random.randint(1, 10))
-s = {}.fromkeys(a)
-b = list(s.keys())
-if len(b) < len(a):
-    print('Повторов: ', len(a)-len(b), a)
-else:
-    print('Повторов нет')
-
-
-#3
